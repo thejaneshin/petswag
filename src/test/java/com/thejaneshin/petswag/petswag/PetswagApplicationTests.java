6626479000cd86c3b55c7966affc71028c1ba862
Jane Shin <thejaneshin@gmail.com>
2019-07-31 10:24:24 -0700

    first commit

---
+++ src/test/java/com/thejaneshin/petswag/petswag/PetswagApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thejaneshin.petswag.petswag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetswagApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
